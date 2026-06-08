83b0cef79b87ee727e95d69dcbb890e9592871d4
behaviodd <behaviodd@gmail.com>
2026-06-08 17:11:18 +0900

    chore: 임시 스크린샷 스크립트 제거

--- screenshot3.mjs
+++
@@ -1,8 +0,0 @@
-import { chromium } from './node_modules/playwright/index.mjs';
-const browser = await chromium.launch({ headless: true });
-const page = await browser.newPage();
-await page.setViewportSize({ width: 1440, height: 900 });
-await page.goto('http://localhost:5174/team', { waitUntil: 'networkidle' });
-await page.screenshot({ path: '/tmp/ss_team_full.png', fullPage: true });
-console.log('done');
-await browser.close();
